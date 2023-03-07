660cbfaa88524025ce08a549fb5354a09885b96b
ViniciusBarros2104 <rm97824@fiap.com.br>
2023-03-07 11:44:10 -0300

    Criado arquivo Js com Consoles e comentarios

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// este é um comentario de linha
+/**
+ * Este é
+ um comentario
+  de bloco
+ */
+
+
+console.log("Olá mundo")
+console.error("Bom dia")
+console.warn("Será?")
+console.info("vai ser...")
